0e33f133700f8e59b1769c3db90a8062de4b3658
Gurney Buchanan <gurney.buchanan@gmail.com>
2018-11-11 13:16:34 -0500

    getRepository implemented to get a repository given its name

--- gdettt/src/main/java/org/compass/gdet/GitHubAPIDemo.java
+++ gdettt/src/main/java/org/compass/gdet/GitHubAPIDemo.java
@@ -1,14 +1,20 @@
 package org.compass.gdet;
+import org.kohsuke.github.*;
+import java.util.List;
 
 public class GitHubAPIDemo {
   public static void main( String[] args ) {
     System.out.println("GitHub Data Extration Tool Demo");
     GithubDataExtractionTool git = new GithubDataExtractionTool();
-    if (git.checkConnection()) {
-      System.out.println("Successfully Established Connection");
-    }
-    else {
+    if (!git.checkConnection()) {
       System.out.println("Error Establishing Connection");
+    }
+    System.out.println("Successfully Established Connection");
+    GHRepository repo = git.getRepository(
+      "CompassSoftware/GDET-Tremendous-Trio");
+    if (repo != null) {
+      System.out.printf("Repository: %s\n", repo.getName());
+      System.out.printf("Owned by: %s\n", repo.getOwnerName());
     }
   }
 }
--- gdettt/src/main/java/org/compass/gdet/GithubDataExtractionTool.java
+++ gdettt/src/main/java/org/compass/gdet/GithubDataExtractionTool.java
@@ -1,11 +1,17 @@
 package org.compass.gdet;
 import org.kohsuke.github.*;
 import java.io.IOException;
+import java.util.List;
 
 public class GithubDataExtractionTool
 {
   private GitHub github;
 
+  /**Constructor
+  * This constructor will try to establish a connection to gthub with the
+  * username and password held in ~/.github and will print an error message
+  * if it is unable to.
+  */
   public GithubDataExtractionTool()
   {
     try {
@@ -16,6 +22,13 @@ public class GithubDataExtractionTool
     }
   }
 
+  /**checkConnection
+  * This method will check the connection to github and will return false if a
+  * connection has not been established.
+  *
+  * @return:
+  *   boolean - true if connection exists, false if not.
+  */
   public boolean checkConnection() {
     try {
       if (github != null) {
@@ -28,6 +41,26 @@ public class GithubDataExtractionTool
     }
     catch (IOException e) {
       return false;
+    }
+  }
+
+  /**getRepository
+  * This method will try to get a repository with the name <reponame> and return
+  * a GHRepository object if successful or null if not.
+  *
+  * @params:
+  *   reponame - the string name of a repo in the standard github format
+  *     <username>/<reponame>
+  *
+  * @return:
+  *   GHRepository - a Github repository object if successful, null if not.
+  */
+  public GHRepository getRepository(String reponame) {
+    try {
+      return github.getRepository(reponame);
+    }
+    catch (IOException e) {
+      return null;
     }
   }
 }
