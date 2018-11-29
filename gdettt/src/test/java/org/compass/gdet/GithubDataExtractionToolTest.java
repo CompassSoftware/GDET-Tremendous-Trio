b812e0a5837bbea2074d86c63f5afc8e780067e6
SupahSprinkle <supahsprinkle@DESKTOP-Q37O9M4.localdomain>
2018-11-29 15:48:14 -0500

    Created test for commit comment to string method that verifies against a known commit comment.

--- gdettt/src/test/java/org/compass/gdet/GithubDataExtractionToolTest.java
+++ gdettt/src/test/java/org/compass/gdet/GithubDataExtractionToolTest.java
@@ -140,6 +140,29 @@ public class GithubDataExtractionToolTest
       "--------------------------------\n\n";
     assertTrue(expected.equals(result));
   }
+    /*
+  * A basic test for our commitToString method.  Should get a valid formatted
+  * string representation of our commit list.
+  */
+  @Test
+  public void shouldGetAFormattedCommitCommentString() {
+    GithubDataExtractionTool gdet = new GithubDataExtractionTool();
+    GHRepository repo =
+      gdet.getRepository("CompassSoftware/GDET-Tremendous-Trio");
+    List<GHCommitComment> cComment = GithubDataExtractionTool.getCommitComments(repo);
+    String cString = GithubDataExtractionTool.commitCommentToString(cComment.get(0));
+    String expected = "--------------------------------\n" +
+  	"SupahSprinkle\n\n" +
+	"Commit Details:\n" +
+	"--------------------------------\n" +
+	"Gurney Buchanan\n" +
+	"Sat Nov 24 23:13:53 GMT 2018\n" +
+	"Added getIssues and issuesToString methods to handle issue information along with tests and demo\n" +
+	"--------------------------------\n\n" +
+	"This is a test of iss17\n" +
+	"--------------------------------\n\n";
+    assertTrue(expected.equals(cString));
+  }
 
   /*
   * A basic test for our getIssues method.  Should get a list of valid issues.
